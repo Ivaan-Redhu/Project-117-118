c79af984c37d54062079dc43840d0e49619f6ab2
Ivaan-Redhu <93902463+Ivaan-Redhu@users.noreply.github.com>
2023-07-01 21:47:31 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -8,3 +8,37 @@ timer_check="";
 drawn_sketch="";
 answer_holder="";
 score=0;
+sketch = Elemental_of_array;
+
+function draw(){
+    check_sketch();
+    if(drawn_sketch == sketch){
+        answer_holder = "set";
+        score = score+1;
+        document.getElementById("score").innerHTML = "Score: "+score;
+    }
+}
+
+function check_sketch(){
+    timer_counter++;
+    document.getElementById("timer").innerHTML = "Timer: "+timer_counter;
+    if(timer_counter>400){
+        timer_counter = 0;
+        timer_check = "completed";
+    }
+    if(timer_check == "completed" || answer_holder == "set"){
+        timer_check = "";
+        answer_holder = "";
+        updateCanvas();
+    }
+}
+
+function updateCanvas(){
+    background("white");
+}
+
+function setup(){
+    canvas=createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
